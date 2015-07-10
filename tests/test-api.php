5dd1792dd4de7825f447d865493e0d1ebcc38a1b
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-07-10 11:36:13 -0400

    load in apikey file

--- tests/test-api.php
+++ tests/test-api.php
@@ -1,17 +1,21 @@
 <?php
 
 class ApiTest extends WP_UnitTestCase {
-    static $config = array(
-        'libraryType' => 'group',
-        'libraryID' => 359247,
-        'librarySlug' => 'wordpress_sync_test_data',
-        'apiKey' => '',
-        'collectionKey' => 'IPJWZUAI',
-    );
+    function get_config() {
+        $api_key = file_get_contents( 'api-key.txt' );
+        $config = array(
+            'libraryType' => 'group',
+            'libraryID' => 359247,
+            'librarySlug' => 'wordpress_sync_test_data',
+            'apiKey' => $api_key,
+            'collectionKey' => 'IPJWZUAI',
+        );
+        return $config;
+    }
 
 	function test_get_items_from_api() {
         global $WP_Zotero_Sync_Plugin;
-        $items = $WP_Zotero_Sync_Plugin->get_items(static::$config);
+        $items = $WP_Zotero_Sync_Plugin->get_items( $this->get_config() );
 		$this->assertEqual( 21, count($items) );
 	}
     

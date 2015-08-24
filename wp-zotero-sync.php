4222f2c07e0d5e24b62cad171ad91778cc25711e
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-08-23 23:24:57 -0400

    properly note new version

--- wp-zotero-sync.php
+++ wp-zotero-sync.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP Zotero Sync
-Version: 0.1-alpha
+Version: 0.1.1
 Description: Syncs Zotero items into a custom "Publications" post type.
 Author: Cora Johnson-Roberson
 Author URI: http://www.corajr.com
